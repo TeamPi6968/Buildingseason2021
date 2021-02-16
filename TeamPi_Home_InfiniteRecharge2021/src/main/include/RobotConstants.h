ad55d3c3089013baabe7e3cca681874a3ca164d0
CalebK <caleb.kyei@gmail.com>
2021-02-16 16:21:58 +0100

    Update Loader Constants

---
+++ TeamPi_Home_InfiniteRecharge2021/src/main/include/RobotConstants.h
@@ -0,0 +1,18 @@
+/**
+ * Made for Robotics TeamPi 2021 Build Season
+ * Date: 10/02/2021
+ * 
+ * Remarks: This file contains the values of all constants and defines
+ *
+ * 
+ **/ 
+
+#pragma once
+
+//Loader Specifications
+const double LoaderMotorSpeed = 0.5;
+
+//Loader Motors
+
+//Loader Sensors
+
