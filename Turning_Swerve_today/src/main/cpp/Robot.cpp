a7170594bcd0e8cfd88be0b25446cd70179f6b22
CalebK <caleb.kyei@gmail.com>
2021-03-04 00:38:09 +0100

    Test 1 - encoder value, DriveMotor

--- Turning_Swerve_today/src/main/cpp/Robot.cpp
+++ Turning_Swerve_today/src/main/cpp/Robot.cpp
@@ -7,6 +7,7 @@
 #include <frc/TimedRobot.h>
 #include <ctre/Phoenix.h>
 
+
 /**
  * This sample program shows how to control a motor using a joystick. In the
  * operator control part of the program, the joystick is read and the value is
@@ -32,18 +33,21 @@ class Robot: public frc::TimedRobot {
 
  
 
-  float limx = 0.2;
-  float limy = 0.2;
+  double limx = 0.2;
+  double limy = 0.2;
 
 
  public:
-  float x;
-  float y;
+  double x;
+  double y;
   double angleCoef = 0.00837209302;
+  double motorCoef = 0.00837209302; //TBD
   frc::Joystick m_stick{0};
 
-  float Stick_limx(){
-    float xvalue;
+  
+
+  double Stick_limx(){
+    double xvalue;
     if(m_stick.GetX() < -limx){
       xvalue = -limx;
     } 
@@ -56,8 +60,8 @@ class Robot: public frc::TimedRobot {
     return xvalue;
   }
 
-  float Stick_limy(){
-    float yvalue;
+  double Stick_limy(){
+    double yvalue;
     if(m_stick.GetY() < -limy){
       yvalue = -limy;
     } 
@@ -141,6 +145,88 @@ class Robot: public frc::TimedRobot {
   //  std::cout << "ONE "<< pos1<<" \n"; 
     // std::cout << pos2;
 
+  }
+
+  void AutonomousInit() override{
+    Motor1->SetSelectedSensorPosition(0,0,50);  
+    Motor2->SetSelectedSensorPosition(0,0,50);
+    Motor3->SetSelectedSensorPosition(0,0,50);
+    Motor4->SetSelectedSensorPosition(0,0,50);
+
+    // Motor2->Follow(*Motor1);
+    // Motor3->Follow(*Motor1);
+    // Motor4->Follow(*Motor1);
+
+    Motor1->ConfigSelectedFeedbackCoefficient(motorCoef,0,50);
+    Motor2->ConfigSelectedFeedbackCoefficient(motorCoef,0,50);
+    Motor3->ConfigSelectedFeedbackCoefficient(motorCoef,0,50);
+    Motor4->ConfigSelectedFeedbackCoefficient(motorCoef,0,50);
+
+
+    Angle1->SetSelectedSensorPosition(0,0,50);  
+    Angle2->SetSelectedSensorPosition(0,0,50);
+    Angle3->SetSelectedSensorPosition(0,0,50);
+    Angle4->SetSelectedSensorPosition(0,0,50);
+
+    Angle1->ConfigSelectedFeedbackCoefficient(angleCoef,0,50);
+    Angle2->ConfigSelectedFeedbackCoefficient(angleCoef,0,50);
+    Angle3->ConfigSelectedFeedbackCoefficient(angleCoef,0,50);
+    Angle4->ConfigSelectedFeedbackCoefficient(angleCoef,0,50);
+
+  }
+  void AutonomousPeriodic() override{
+
+
+    Motor1->Config_kP(0,0.5);
+    Motor1->Config_kI(0,0);
+    Motor1->Config_kD(0,0.1);
+    Motor1->Set(ControlMode::Position,360); //Must check
+
+    Motor2->Config_kP(0,0.5);
+    Motor2->Config_kI(0,0);
+    Motor2->Config_kD(0,0.1);
+    Motor2->Set(ControlMode::Position,360);
+
+    Motor3->Config_kP(0,0.5);
+    Motor3->Config_kI(0,0);
+    Motor3->Config_kD(0,0.1);
+    Motor3->Set(ControlMode::Position,360);
+
+    Motor4->Config_kP(0,0.5);
+    Motor4->Config_kI(0,0);
+    Motor4->Config_kD(0,0.1);
+    Motor4->Set(ControlMode::Position,360);
+
+    double pos1 = Motor1->GetSelectedSensorPosition();
+    double pos2 = Motor2->GetSelectedSensorPosition();
+    // double pos3 = Angle3->GetSelectedSensorPosition();
+    // double pos4 = Angle4->GetSelectedSensorPosition();
+
+    std::cout << "ONE "<< pos1 <<" \n"; 
+    std::cout << "TWO "<< pos2<<" \n"; 
+  //  std::cout << "ONE "<< pos1<<" \n"; 
+  //  std::cout << "ONE "<< pos1<<" \n"; 
+    // std::cout << pos2;
+
+    Angle1->Config_kP(0,0.5);
+    Angle1->Config_kI(0,0);
+    Angle1->Config_kD(0,0.1);
+    Angle1->Set(ControlMode::Position, 720);
+
+    Angle2->Config_kP(0,0.5);
+    Angle2->Config_kI(0,0);
+    Angle2->Config_kD(0,0.1);
+    Angle2->Set(ControlMode::Position, 720);
+
+    Angle3->Config_kP(0,0.5);
+    Angle3->Config_kI(0,0);
+    Angle3->Config_kD(0,0.1);
+    Angle3->Set(ControlMode::Position, 720);
+
+    Angle4->Config_kP(0,0.5);
+    Angle4->Config_kI(0,0);
+    Angle4->Config_kD(0,0.1);
+    Angle4->Set(ControlMode::Position, 720);
   }
 };
 
