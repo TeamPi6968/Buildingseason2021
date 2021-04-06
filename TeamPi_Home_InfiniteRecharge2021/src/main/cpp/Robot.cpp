943c3b01e9c8fa3558763419cc1f77b7ed67dd17
M4R1U517 <74927674+M4R1U517@users.noreply.github.com>
2021-04-06 22:20:46 +0200

    changes
    stashed changes

--- TeamPi_Home_InfiniteRecharge2021/src/main/cpp/Robot.cpp
+++ TeamPi_Home_InfiniteRecharge2021/src/main/cpp/Robot.cpp
@@ -4,10 +4,7 @@
 
 #include "Robot.h"
 
-void Robot::RobotInit() {
-  Swerve.SetAllPID();
-  Swerve.ZeroDrivetrain();
-}
+void Robot::RobotInit() {}
 void Robot::RobotPeriodic() {}
 
 void Robot::AutonomousInit() {
@@ -15,17 +12,55 @@ void Robot::AutonomousInit() {
 
 }
 void Robot::AutonomousPeriodic() {
+<<<<<<< HEAD
+=======
   DP.Bounce();
+<<<<<<< Updated upstream
+=======
+>>>>>>> c6ef59513552808defe2e4b7afec50742d444691
+>>>>>>> Stashed changes
 
 }
 
 void Robot::TeleopInit() {
+<<<<<<< Updated upstream
 //  pixy = new Get_Pixy2(frc::I2C::kOnboard, 8);
+=======
+<<<<<<< HEAD
+  // frc::DoubleSolenoid anotherDoubleSolenoid{0, 1, 2};
+
+  // anotherDoubleSolenoid.Set(frc::DoubleSolenoid::Value::kForward);
+
+=======
+//  pixy = new Get_Pixy2(frc::I2C::kOnboard, 8);
+>>>>>>> c6ef59513552808defe2e4b7afec50742d444691
+>>>>>>> Stashed changes
   Swerve.SetAllPID();
   Swerve.ZeroDrivetrain();
-
 }
 
+<<<<<<< HEAD
+
+void Robot::TeleopPeriodic() {
+  Turret.Calibration(m_stickTurret.GetRawButtonPressed(ButtonSTART));
+  if (Turret.Calibrated)
+  {
+    Turret.Home(m_stickTurret.GetRawButton(L3), m_stickTurret.GetRawButton(R3));
+    Turret.Move(m_stickTurret.GetX(), m_stickTurret.GetRawAxis(5));
+
+//   //   ModTurret.Shoot(m_stickTurret.GetRawButtonPressed(ButtonB),IRSensor.Get());
+//   //   ModTurret.simulating(m_stickTurret.GetRawButtonPressed(ButtonX),m_stickTurret.GetRawButtonPressed(ButtonY),m_stickTurret.GetRawButtonPressed(ButtonA));
+    
+//     // Swerve.Drive(m_stick.GetX(), -m_stick.GetY(), m_stick.GetRawAxis(4));
+
+//     // ModIntake.IntakeSolonoids(m_stick.GetRawButtonPressed(ButtonRB),ModTurret.AmountCellsInStorage); //6 = right top trigger
+//     // ModTurret.LoaderStoreLoad(m_stick.GetRawButtonPressed(ButtonLB));
+//     // ModTurret.refillTurret(ModTurret.SimulatingIRSensor);
+
+//     // ModTurret.test(m_stick.GetRawButton(ButtonX));
+  }
+}
+=======
 void Robot::TeleopPeriodic() {
   Swerve.Drive(m_stick.GetX(), -m_stick.GetY(), m_stick.GetRawAxis(4));
 
@@ -41,16 +76,28 @@ void Robot::TeleopPeriodic() {
   ModTurret.simulating(m_stick.GetRawButtonPressed(ButtonX),m_stick.GetRawButtonPressed(ButtonY),m_stick.GetRawButtonPressed(ButtonA));
   }
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> c6ef59513552808defe2e4b7afec50742d444691
+>>>>>>> Stashed changes
 
 void Robot::DisabledInit() {}
 void Robot::DisabledPeriodic() {}
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+=======
+>>>>>>> Stashed changes
 void Robot::TestInit() {
   DP.ClearFile();
 }
 void Robot::TestPeriodic() {
   DP.RecBounce();
 }
+>>>>>>> c6ef59513552808defe2e4b7afec50742d444691
 
 
 #ifndef RUNNING_FRC_TESTS
--- TeamPi_Home_InfiniteRecharge2021/src/main/include/Robot.h
+++ TeamPi_Home_InfiniteRecharge2021/src/main/include/Robot.h
@@ -7,28 +7,50 @@
 #include <frc/TimedRobot.h>
 #include <frc/Joystick.h>
 
-#include <frc/DigitalInput.h>
+//#include <frc/DigitalInput.h>
+#include "Outtake.h"
 #include "Intake_Storage_Loader/Loader.h"
 #include "SwerveDrive/Drivetrain.h"
 #include "Intake_Storage_Loader/Intake.h"
 
+<<<<<<< Updated upstream
 #include "SwerveDrive/DrivePattern.h"
 #include "Intake_Storage_Loader/C04_PI_Pixy.h"
+=======
+<<<<<<< HEAD
+#include <Pixy2/Pixy2CCC.h>
+//#include <PixyProgram.h>
+=======
+#include "SwerveDrive/DrivePattern.h"
+#include "Intake_Storage_Loader/C04_PI_Pixy.h"
+>>>>>>> c6ef59513552808defe2e4b7afec50742d444691
+>>>>>>> Stashed changes
 
 
 
 class Robot : public frc::TimedRobot {
  private:
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> Stashed changes
 //  Pixy2Program* ball_detect;
 
 //  Get_Pixy2 *pixy;
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> c6ef59513552808defe2e4b7afec50742d444691
+>>>>>>> Stashed changes
   Drivetrain Swerve;
   frc::Joystick m_stick{0};
+  frc::Joystick m_stickTurret{1};
   Intake ModIntake;
   Loader ModTurret;
+  Outtake Turret;
   Loader test;
-  DrivePattern DP;
+  
 
 int ButtonA = 1;
 int ButtonB = 2;
@@ -38,6 +60,8 @@ int ButtonLB = 5;
 int ButtonRB = 6;
 int ButtonBACK = 7;
 int ButtonSTART = 8;
+int L3 = 9;
+int R3 = 10;
 
 
  public:
